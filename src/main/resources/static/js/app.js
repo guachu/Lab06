0a512dfeb7b1d414b16beffc6aac798db9a6bfbc
Alejandra Gómez <alejita.9813.dg@gmail.com>
2019-03-20 22:55:39 -0500

    avance con error

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,47 +1,42 @@
-$('#dynamictable').append('<table></table>');
-var table = $('#dynamictable').children();    
-table.append("<tr><td>a</td><td>b</td></tr>");
-table.append("<tr><td>c</td><td>d</td></tr>");
-
-
-
-$(function () {
-    $("#btnGenerate").click(function () {
-        //Build an array containing Customer records.
-        var customers = new Array();
-        customers.push(["Customer Id", "Name", "Country"]);
-        customers.push([1, "John Hammond", "United States"]);
-        customers.push([2, "Mudassar Khan", "India"]);
-        customers.push([3, "Suzanne Mathews", "France"]);
-        customers.push([4, "Robert Schidner", "Russia"]);
- 
-        //Create a HTML Table element.
-        var table = $("<table />");
-        table[0].border = "1";
- 
-        //Get the count of columns.
-        var columnCount = customers[0].length;
- 
-        //Add the header row.
-        var row = $(table[0].insertRow(-1));
-        for (var i = 0; i < columnCount; i++) {
-            var headerCell = $("<th />");
-            headerCell.html(customers[0][i]);
-            row.append(headerCell);
-        }
- 
-        //Add the data rows.
-        for (var i = 1; i < customers.length; i++) {
-            row = $(table[0].insertRow(-1));
-            for (var j = 0; j < columnCount; j++) {
-                var cell = $("<td />");
-                cell.html(customers[i][j]);
-                row.append(cell);
-            }
-        }
- 
-        var dvTable = $("#dvTable");
-        dvTable.html("");
-        dvTable.append(table);
-    });
-});
+var c=(function(){
+	function crear(){
+		var cinema = document.getElementById("cinema").value;
+		var tabla= $('#Cine');
+		var html;
+		
+		CinemaByname =function(cine){
+			 var tabla2=$('#funcion');
+			 html="";
+			 tabla2.empty();
+			 if(cine!=null){
+					var funciones=cine[0].functions;				
+					tabla2.append(html);
+					for(var i =0;i<funciones.length;i++){
+						var asientos=funciones[i].seats.length*funciones[i].seats[0].length;
+						html='<tr>';
+						html+='<td>'+funciones[i].movie.name + '</td>';
+						html+='<td>'+funciones[i].genre + '</td>';
+						html+='<td>'+funciones[i].date + '</td>';
+						html+='</tr>'
+						tabla2.append(html);					
+					}				
+			 }
+			 else{
+				 alert("cine no encontrado");
+			 }
+				 
+		}
+	};
+	
+	return {		
+		metodoNombre:function (){
+			var movie= $('#movie').val();
+			apimock.getCinemaByName(movie,CinemaByname);			
+		},
+		revisar:function () {
+			var chequear=document.crear.getElementById('cinema');
+			console.log( chequear);		
+	    }	
+		
+	}
+})();
